41891532bc43de22155cdb6af24085aefd73070b
Jack Wohlfert <jack@surge4.com>
2016-07-08 12:10:07 -0400

    Add keyboard type prop

--- index.js
+++ index.js
@@ -149,7 +149,7 @@ class EmailInput extends Component {
               width: width,
               color: inputColor
             }
-          ]} underlineColorAndroid="rgba(0,0,0,0)" onChange={this.onChange.bind(this)} onSubmitEditing={this.parseEmails}/>
+          ]} keyboardType={this.props.keyboardType || "email-address"} underlineColorAndroid="rgba(0,0,0,0)" onChange={this.onChange.bind(this)} onSubmitEditing={this.parseEmails}/>
         </View>
       </View>
     </TouchableWithoutFeedback>
