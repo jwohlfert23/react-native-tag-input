6cc488cc9410ff426afbd1aa926171a9302a7652
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2016-11-07 19:19:23 -0200

    add eslint

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, {PropTypes, Component} from 'react';
+import React, { PropTypes, Component } from 'react';
 import {
   View,
   Text,
@@ -6,19 +6,34 @@ import {
   StyleSheet,
   TouchableOpacity,
   Dimensions,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
 } from 'react-native';
 import _ from 'lodash';
 
+const { width } = Dimensions.get('window');
+
 class EmailInput extends Component {
+  static defaultProps = {
+    onChangeText: () => {},
+  };
+
+  static propTypes = {
+    onChange: PropTypes.func.isRequired,
+    value: PropTypes.array.isRequired,
+    onChangeText: PropTypes.func,
+    regex: PropTypes.object,
+    tagColor: PropTypes.string,
+    tagTextColor: PropTypes.string,
+    inputColor: PropTypes.string,
+  }
+
+  state = {
+    text: '',
+    inputWidth: 200,
+  };
+
   constructor(props) {
     super(props);
-    let {height, width} = Dimensions.get('window');
-
-    this.state = {
-      text: "",
-      inputWidth: 200
-    };
 
     this.wrapperWidth = width;
     this.parseEmails = this.parseEmails.bind(this);
@@ -27,170 +42,182 @@ class EmailInput extends Component {
     this.calculateWidth = this.calculateWidth.bind(this);
     this.focus = this.focus.bind(this);
   }
+
   measureWrapper() {
     if (!this.refs.wrapper)
       return;
-    this.refs.wrapper.measure((ox, oy, w, h, px, py) => {
+
+    this.refs.wrapper.measure((ox, oy, w, /*h, px, py*/) => {
       this.wrapperWidth = w;
     });
   }
-  calculateWidth() {
 
+  calculateWidth() {
     setTimeout(() => {
       if (!this.refs['tag' + (this.props.value.length - 1)])
         return;
-      this.refs['tag' + (this.props.value.length - 1)].measure((ox, oy, w, h, px, py) => {
 
-        let endPosOfTag = w + ox;
-        let margin = 3;
-        let spaceLeft = this.wrapperWidth - endPosOfTag - margin;
+      this.refs['tag' + (this.props.value.length - 1)].measure((ox, oy, w, /*h, px, py*/) => {
+        const endPosOfTag = w + ox;
+        const margin = 3;
+        const spaceLeft = this.wrapperWidth - endPosOfTag - margin;
+
         if (spaceLeft < 100) {
-          this.setState({inputWidth: this.wrapperWidth});
+          this.setState({ inputWidth: this.wrapperWidth });
         } else {
-          this.setState({inputWidth: spaceLeft});
+          this.setState({ inputWidth: spaceLeft });
         }
       });
     }, 0);
   }
+
   componentDidMount() {
     setTimeout(() => {
       this.calculateWidth();
     }, 100);
   }
+
   componentWillReceiveProps() {
     this.calculateWidth();
   }
-  componentDidUpdate(prevProps, prevState) {
+
+  componentDidUpdate(prevProps, /*prevState*/) {
     if (prevProps.value.length != this.props.value.length || !prevProps.value) {
       this.calculateWidth();
     }
   }
+
   onChange(event) {
     if (!event || !event.nativeEvent)
       return;
-    let text = event.nativeEvent.text;
-    this.setState({text: text});
-    let lastTyped = text.charAt(text.length - 1);
 
-    let parseWhen = [",", " ", ";"];
+    const text = event.nativeEvent.text;
+    this.setState({ text: text });
+    const lastTyped = text.charAt(text.length - 1);
+
+    const parseWhen = [',', ' ', ';'];
 
     if (parseWhen.indexOf(lastTyped) > -1)
       this.parseEmails();
     }
 
   parseEmails() {
-    let {text} = this.state;
-    let {value} = this.props;
-    let regex = this.props.regex || /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
-    let results = text.match(regex);
+    const { text } = this.state;
+    const { value } = this.props;
+
+    const regex = this.props.regex || /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
+    const results = text.match(regex);
 
     if (results && results.length > 0) {
-      this.setState({text: ""});
+      this.setState({ text: '' });
       this.props.onChange(value.concat(results));
     }
-
   }
+
   onKeyPress(event) {
-    if (this.state.text === "" && event.nativeEvent && event.nativeEvent.key == "Backspace") {
+    if (this.state.text === '' && event.nativeEvent && event.nativeEvent.key == 'Backspace') {
       this.pop();
     }
   }
+
   focus() {
     if (this.refs.emailInput)
       this.refs.emailInput.focus();
-    }
+  }
+
   pop() {
-    let emails = _.clone(this.props.value);
+    const emails = _.clone(this.props.value);
     emails.pop();
     this.props.onChange(emails);
     this.focus();
   }
+
   removeIndex(index) {
-    let emails = _.clone(this.props.value);
+    const emails = _.clone(this.props.value);
     emails.splice(index, 1);
     this.props.onChange(emails);
     this.focus();
-
   }
-  render() {
-    let {text} = this.state;
-    let {value, inputProps} = this.props;
 
-    let defaultInputProps = {
-      autoCapitalize: "none",
+  render() {
+    const { text } = this.state;
+    const { value } = this.props;
+    let { inputProps } = this.props;
+
+    const defaultInputProps = {
+      autoCapitalize: 'none',
       autoCorrect: false,
-      placeholder: this.props.placeholder || "Start typing",
-      returnKeyType: this.props.returnKeyType || "done",
-      keyboardType: this.props.keyboardType || "email-address",
-      underlineColorAndroid: "rgba(0,0,0,0)"
+      placeholder: this.props.placeholder || 'Start typing',
+      returnKeyType: this.props.returnKeyType || 'done',
+      keyboardType: this.props.keyboardType || 'email-address',
+      underlineColorAndroid: 'rgba(0,0,0,0)',
     }
 
-    inputProps = {...defaultInputProps, ...inputProps};
+    inputProps = { ...defaultInputProps, ...inputProps };
 
-    let width = text.length < 4 ? 100 : null;
+    const width = text.length < 4 ? 100 : null;
 
-    let tagColor = this.props.tagColor || "#dddddd";
-    let tagTextColor = this.props.tagTextColor || "#777777";
-    let inputColor = this.props.inputColor || "#777777";
+    const tagColor = this.props.tagColor || '#dddddd';
+    const tagTextColor = this.props.tagTextColor || '#777777';
+    const inputColor = this.props.inputColor || '#777777';
 
-    return <TouchableWithoutFeedback onPress={() => this.refs.emailInput.focus()} onLayout={this.measureWrapper.bind(this)} style={{
-      flex: 1
-    }}>
-      <View style={styles.wrapper} ref="wrapper" onLayout={this.measureWrapper.bind(this)}>
-        {value.map((email, index) => {
-          return <TouchableOpacity key={index} ref={"tag" + index} style={[
-            styles.tag, {
-              backgroundColor: tagColor
-            }
-          ]} onPress={this.removeIndex.bind(this, index)}>
-            <Text style={[
-              styles.tagText, {
-                color: tagTextColor
-              }
-            ]}>{email}&nbsp;&times;</Text>
-          </TouchableOpacity>
-        })}
-
-        <View style={{
-          width: this.state.inputWidth,
-          height: 36
-        }}>
-          <TextInput ref="emailInput" {...inputProps} blurOnSubmit={false} onKeyPress={this.onKeyPress.bind(this)} value={this.state.text} style={[
-            styles.textInput, {
-              width: width,
-              color: inputColor
-            }
-          ]} onChange={this.onChange.bind(this)} onSubmitEditing={this.parseEmails}/>
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.refs.emailInput.focus()}
+        onLayout={this.measureWrapper.bind(this)}
+        style={styles.container}>
+        <View
+          style={styles.wrapper}
+          ref="wrapper"
+          onLayout={this.measureWrapper.bind(this)}>
+          {value.map((email, index) => (
+            <TouchableOpacity
+              key={index}
+              ref={'tag' + index}
+              style={[
+                styles.tag, {
+                  backgroundColor: tagColor,
+                }]}
+              onPress={this.removeIndex.bind(this, index)}>
+              <Text style={[
+                styles.tagText, {
+                  color: tagTextColor,
+                },
+              ]}>{email}&nbsp;&times;</Text>
+            </TouchableOpacity>
+          ))}
+          <View style={[styles.textInputContainer, { width: this.state.inputWidth }]}>
+            <TextInput
+              ref="emailInput"
+              {...inputProps}
+              blurOnSubmit={false}
+              onKeyPress={this.onKeyPress.bind(this)}
+              value={this.state.text}
+              style={[styles.textInput, {
+                width: width,
+                color: inputColor,
+              }]}
+              onChange={this.onChange.bind(this)}
+              onChangeText={this.props.onChangeText}
+              onSubmitEditing={this.parseEmails}/>
+          </View>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    )
   }
 }
 
-EmailInput.PropTypes = {
-  onChange: React.PropTypes.func.isRequired,
-  value: React.PropTypes.array.isRequired,
-  regex: React.PropTypes.object,
-  tagColor: React.PropTypes.string,
-  tagTextColor: React.PropTypes.string,
-  inputColor: React.PropTypes.string
-}
-export default EmailInput;
-
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   wrapper: {
     flex: 1,
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginTop: 3,
     marginBottom: 2,
-    alignItems: 'flex-start'
-  },
-  spacer: {
-    flexWrap: 'wrap',
-    height: 36,
-    width: 0,
-    backgroundColor: "red"
+    alignItems: 'flex-start',
   },
   textInput: {
     height: 36,
@@ -200,18 +227,22 @@ const styles = StyleSheet.create({
     padding: 0,
     flexWrap: 'wrap',
   },
+  textInputContainer: {
+    height: 36,
+  },
   tag: {
-    padding: 0,
     justifyContent: 'center',
     marginTop: 6,
     marginRight: 3,
     flexWrap: 'wrap',
     padding: 8,
     height: 24,
-    borderRadius: 2
+    borderRadius: 2,
   },
   tagText: {
     padding: 0,
-    margin: 0
-  }
-})
+    margin: 0,
+  },
+});
+
+export default EmailInput;
