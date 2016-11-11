6a54ba4b75e30c45d56fee8a89a83c64bbee3d67
Sibelius Seraphini <sibeliusseraphini@gmail.com>
2016-11-11 13:23:05 -0200

    Huge refactor
    - use flow types instead of proptypes
    - use ScrollView to keep tagInput with a predefined numberOfLines
    - fix TextInput width estimation
    - add labelKey prop to enable passing objects as tags
    - numberOfLines props to set the maximum number of lines that this component should have
    
    it is much like the tag input of facebook app

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import React, {PropTypes, Component} from 'react';
+// @flow
+
+import React, { Component } from 'react';
 import {
   View,
   Text,
@@ -6,191 +8,307 @@ import {
   StyleSheet,
   TouchableOpacity,
   Dimensions,
-  TouchableWithoutFeedback
+  TouchableWithoutFeedback,
+  ScrollView,
 } from 'react-native';
 import _ from 'lodash';
 
-class EmailInput extends Component {
-  constructor(props) {
-    super(props);
-    let {height, width} = Dimensions.get('window');
+const { width } = Dimensions.get('window');
 
-    this.state = {
-      text: "",
-      inputWidth: 200
-    };
+type Props = {
+  /**
+   * A handler to be called when array of tags change
+   */
+    onChange: (items: Array<any> ) => void,
+  /**
+   * An array of tags
+   */
+    value: Array<any>,
+  /**
+   * A RegExp to test tags after enter, space, or a comma is pressed
+   */
+    regex?: Object,
+  /**
+   * Background color of tags
+   */
+    tagColor?: string,
+  /**
+   * Text color of tags
+   */
+    tagTextColor?: string,
+  /**
+   * Color of text input
+   */
+    inputColor?: string,
+  /**
+   * TextInput props Text.propTypes
+   */
+    inputProps?: Object,
+  /**
+   * path of the label in tags objects
+   */
+    labelKey?: string,
+  /**
+   *  maximum number of lines of this component
+   */
+    numberOfLines: number,
+};
 
-    this.wrapperWidth = width;
-    this.parseEmails = this.parseEmails.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.pop = this.pop.bind(this);
-    this.calculateWidth = this.calculateWidth.bind(this);
-    this.focus = this.focus.bind(this);
-  }
-  measureWrapper() {
+type State = {
+  text: string,
+  inputWidth: ?number,
+  lines: number,
+};
+
+type NativeEvent = {
+  target: number,
+  key: string,
+  eventCount: number,
+  text: string,
+};
+
+type Event = {
+  nativeEvent: NativeEvent,
+};
+
+class TagInput extends Component {
+  props: Props;
+  state: State = {
+    text: '',
+    inputWidth: null,
+    lines: 1,
+  };
+
+  wrapperWidth = width;
+
+  // scroll to bottom
+  contentHeight: 0;
+  scrollViewHeight: 0;
+
+  static defaultProps = {
+    tagColor: '#dddddd',
+    tagTextColor: '#777777',
+    inputColor: '#777777',
+    numberOfLines: 2,
+  };
+
+  measureWrapper = () => {
     if (!this.refs.wrapper)
       return;
-    this.refs.wrapper.measure((ox, oy, w, h, px, py) => {
-      this.wrapperWidth = w;
-    });
-  }
-  calculateWidth() {
 
+    this.refs.wrapper.measure((ox, oy, w, /*h, px, py*/) => {
+      this.wrapperWidth = w;
+      this.setState({ inputWidth: this.wrapperWidth });
+    });
+  };
+
+  calculateWidth = () => {
     setTimeout(() => {
       if (!this.refs['tag' + (this.props.value.length - 1)])
         return;
-      this.refs['tag' + (this.props.value.length - 1)].measure((ox, oy, w, h, px, py) => {
 
-        let endPosOfTag = w + ox;
-        let margin = 3;
-        let spaceLeft = this.wrapperWidth - endPosOfTag - margin;
+      this.refs['tag' + (this.props.value.length - 1)].measure((ox, oy, w, /*h, px, py*/) => {
+        const endPosOfTag = w + ox;
+        const margin = 3;
+        const spaceLeft = this.wrapperWidth - endPosOfTag - margin - 10;
+
+        const inputWidth = (spaceLeft < 100) ? this.wrapperWidth : spaceLeft - 10;
+
         if (spaceLeft < 100) {
-          this.setState({inputWidth: this.wrapperWidth});
+          if (this.state.lines < this.props.numberOfLines) {
+            const lines = this.state.lines + 1;
+
+            this.setState({ inputWidth, lines });
+          } else {
+            this.setState({ inputWidth }, () => this.scrollToBottom());
+          }
         } else {
-          this.setState({inputWidth: spaceLeft});
+          this.setState({ inputWidth });
         }
       });
     }, 0);
-  }
+  };
+
   componentDidMount() {
     setTimeout(() => {
       this.calculateWidth();
     }, 100);
   }
-  componentWillReceiveProps() {
-    this.calculateWidth();
-  }
-  componentDidUpdate(prevProps, prevState) {
+
+  componentDidUpdate(prevProps: Props, /*prevState*/) {
     if (prevProps.value.length != this.props.value.length || !prevProps.value) {
       this.calculateWidth();
     }
   }
-  onChange(event) {
+
+  onChange = (event: Event) => {
     if (!event || !event.nativeEvent)
       return;
-    let text = event.nativeEvent.text;
-    this.setState({text: text});
-    let lastTyped = text.charAt(text.length - 1);
 
-    let parseWhen = [",", " ", ";"];
+    const text = event.nativeEvent.text;
+    this.setState({ text: text });
+    const lastTyped = text.charAt(text.length - 1);
+
+    const parseWhen = [',', ' ', ';'];
 
     if (parseWhen.indexOf(lastTyped) > -1)
-      this.parseEmails();
-    }
+      this.parseTags();
+  };
 
-  parseEmails() {
-    let {text} = this.state;
-    let {value} = this.props;
-    let regex = this.props.regex || /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
-    let results = text.match(regex);
+  parseTags = () => {
+    const { text } = this.state;
+    const { value } = this.props;
+
+    const regex = this.props.regex || /([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi;
+    const results = text.match(regex);
 
     if (results && results.length > 0) {
-      this.setState({text: ""});
+      this.setState({ text: '' });
       this.props.onChange(value.concat(results));
     }
+  };
 
-  }
-  onKeyPress(event) {
-    if (this.state.text === "" && event.nativeEvent && event.nativeEvent.key == "Backspace") {
+  onKeyPress = (event: Event) => {
+    if (this.state.text === '' && event.nativeEvent && event.nativeEvent.key == 'Backspace') {
       this.pop();
     }
-  }
-  focus() {
-    if (this.refs.emailInput)
-      this.refs.emailInput.focus();
-    }
-  pop() {
-    let emails = _.clone(this.props.value);
-    emails.pop();
-    this.props.onChange(emails);
-    this.focus();
-  }
-  removeIndex(index) {
-    let emails = _.clone(this.props.value);
-    emails.splice(index, 1);
-    this.props.onChange(emails);
-    this.focus();
+  };
 
-  }
+  focus = () => {
+    if (this.refs.tagInput)
+      this.refs.tagInput.focus();
+  };
+
+  pop = () => {
+    const tags = _.clone(this.props.value);
+    tags.pop();
+    this.props.onChange(tags);
+    this.focus();
+  };
+
+  removeIndex = (index: number) => {
+    const tags = _.clone(this.props.value);
+    tags.splice(index, 1);
+    this.props.onChange(tags);
+    this.focus();
+  };
+
+  _getLabelValue = (tag) => {
+    const { labelKey } = this.props;
+
+    if (labelKey) {
+      if (labelKey in tag) {
+        return tag[labelKey];
+      }
+    }
+
+    return tag;
+  };
+
+  _renderTag = (tag, index) => {
+    const { tagColor, tagTextColor } = this.props;
+
+    return (
+      <TouchableOpacity
+        key={index}
+        ref={'tag' + index}
+        style={[styles.tag, { backgroundColor: tagColor }]}
+        onPress={() => this.removeIndex(index)}>
+        <Text style={[styles.tagText, { color: tagTextColor }]}>
+          {this._getLabelValue(tag)}&nbsp;&times;
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
+
+
+  scrollToBottom = (animated: boolean = true) => {
+    if (this.contentHeight > this.scrollViewHeight) {
+      this.refs.scrollView.scrollTo({
+        y: this.contentHeight - this.scrollViewHeight,
+        animated,
+      });
+    }
+  };
+
   render() {
-    let {text} = this.state;
-    let {value, inputProps} = this.props;
+    const { text, inputWidth, lines } = this.state;
+    const { value, inputColor } = this.props;
 
-    let defaultInputProps = {
-      autoCapitalize: "none",
+    const defaultInputProps = {
+      autoCapitalize: 'none',
       autoCorrect: false,
-      placeholder: this.props.placeholder || "Start typing",
-      returnKeyType: this.props.returnKeyType || "done",
-      keyboardType: this.props.keyboardType || "email-address",
-      underlineColorAndroid: "rgba(0,0,0,0)"
+      placeholder: 'Start typing',
+      returnKeyType: 'done',
+      keyboardType: 'default',
+      underlineColorAndroid: 'rgba(0,0,0,0)',
     }
 
-    inputProps = {...defaultInputProps, ...inputProps};
+    const inputProps = { ...defaultInputProps, ...this.props.inputProps };
 
-    let width = text.length < 4 ? 100 : null;
+    const wrapperHeight = (lines - 1) * 40 + 36;
 
-    let tagColor = this.props.tagColor || "#dddddd";
-    let tagTextColor = this.props.tagTextColor || "#777777";
-    let inputColor = this.props.inputColor || "#777777";
+    const width = inputWidth ? inputWidth : 400;
 
-    return <TouchableWithoutFeedback onPress={() => this.refs.emailInput.focus()} onLayout={this.measureWrapper.bind(this)} style={{
-      flex: 1
-    }}>
-      <View style={styles.wrapper} ref="wrapper" onLayout={this.measureWrapper.bind(this)}>
-        {value.map((email, index) => {
-          return <TouchableOpacity key={index} ref={"tag" + index} style={[
-            styles.tag, {
-              backgroundColor: tagColor
-            }
-          ]} onPress={this.removeIndex.bind(this, index)}>
-            <Text style={[
-              styles.tagText, {
-                color: tagTextColor
-              }
-            ]}>{email}&nbsp;&times;</Text>
-          </TouchableOpacity>
-        })}
-
-        <View style={{
-          width: this.state.inputWidth,
-          height: 36
-        }}>
-          <TextInput ref="emailInput" {...inputProps} blurOnSubmit={false} onKeyPress={this.onKeyPress.bind(this)} value={this.state.text} style={[
-            styles.textInput, {
-              width: width,
-              color: inputColor
-            }
-          ]} onChange={this.onChange.bind(this)} onSubmitEditing={this.parseEmails}/>
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => this.refs.tagInput.focus()}
+        onLayout={this.measureWrapper}
+        style={[styles.container]}>
+        <View
+          style={[styles.wrapper,{height: wrapperHeight}]}
+          ref="wrapper"
+          onLayout={this.measureWrapper}>
+          <ScrollView
+            ref='scrollView'
+            style={styles.tagInputContainerScroll}
+            onContentSizeChange={(w, h) => this.contentHeight = h}
+            onLayout={ev => this.scrollViewHeight = ev.nativeEvent.layout.height}
+          >
+            <View style={styles.tagInputContainer}>
+              {value.map((tag, index) => this._renderTag(tag, index))}
+              <View style={[styles.textInputContainer, { width: this.state.inputWidth }]}>
+                <TextInput
+                  ref="tagInput"
+                  {...inputProps}
+                  blurOnSubmit={false}
+                  onKeyPress={this.onKeyPress}
+                  value={text}
+                  style={[styles.textInput, {
+                  width: width,
+                  color: inputColor,
+                }]}
+                  onChange={this.onChange}
+                  onSubmitEditing={this.parseTags}
+                />
+              </View>
+            </View>
+          </ScrollView>
         </View>
-      </View>
-    </TouchableWithoutFeedback>
+      </TouchableWithoutFeedback>
+    )
   }
 }
-
-EmailInput.PropTypes = {
-  onChange: React.PropTypes.func.isRequired,
-  value: React.PropTypes.array.isRequired,
-  regex: React.PropTypes.object,
-  tagColor: React.PropTypes.string,
-  tagTextColor: React.PropTypes.string,
-  inputColor: React.PropTypes.string
-}
-export default EmailInput;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   wrapper: {
     flex: 1,
     flexDirection: 'row',
-    flexWrap: 'wrap',
     marginTop: 3,
     marginBottom: 2,
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
   },
-  spacer: {
+  tagInputContainerScroll: {
+    flex: 1,
+  },
+  tagInputContainer: {
+    flex: 1,
+    flexDirection: 'row',
     flexWrap: 'wrap',
-    height: 36,
-    width: 0,
-    backgroundColor: "red"
   },
   textInput: {
     height: 36,
@@ -198,20 +316,23 @@ const styles = StyleSheet.create({
     flex: .6,
     marginBottom: 6,
     padding: 0,
-    flexWrap: 'wrap',
+
+  },
+  textInputContainer: {
+    height: 36,
   },
   tag: {
-    padding: 0,
     justifyContent: 'center',
     marginTop: 6,
     marginRight: 3,
-    flexWrap: 'wrap',
     padding: 8,
     height: 24,
-    borderRadius: 2
+    borderRadius: 2,
   },
   tagText: {
     padding: 0,
-    margin: 0
-  }
-})
+    margin: 0,
+  },
+});
+
+export default TagInput;
