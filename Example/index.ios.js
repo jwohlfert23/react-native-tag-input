a77044ce15597e4112ebf09fcaffd028164172a9
ashoat <ashoat@gmail.com>
2017-10-18 10:51:47 -0400

    index.ios.js & index.android.js -> index.js

--- Example/index.ios.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import { AppRegistry } from 'react-native';
-import TagInputExample from './src/TagInputExample';
-
-AppRegistry.registerComponent('Example', () => TagInputExample);
