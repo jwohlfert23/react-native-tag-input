560e477cc46f0817b44859825f5783dd1d6a6a18
Evzen Vlk <evzen.vlk@gmail.com>
2021-10-15 13:43:14 +0200

    Rename legacy component lifecycles

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ class TagInput<T> extends React.PureComponent<Props<T>, State> {
     }
   }
 
-  componentWillReceiveProps(nextProps: Props<T>) {
+  UNSAFE_componentWillReceiveProps(nextProps: Props<T>) {
     const inputWidth = TagInput.inputWidth(
       nextProps.text,
       this.spaceLeft,
@@ -187,7 +187,7 @@ class TagInput<T> extends React.PureComponent<Props<T>, State> {
     }
   }
 
-  componentWillUpdate(nextProps: Props<T>, nextState: State) {
+  UNSAFE_componentWillUpdate(nextProps: Props<T>, nextState: State) {
     if (
       this.props.onHeightChange &&
       nextState.wrapperHeight !== this.state.wrapperHeight
@@ -388,7 +388,7 @@ class Tag extends React.PureComponent<TagProps> {
   };
   curPos: ?number = null;
 
-  componentWillReceiveProps(nextProps: TagProps) {
+  UNSAFE_componentWillReceiveProps(nextProps: TagProps) {
     if (
       !this.props.isLastTag &&
       nextProps.isLastTag &&
